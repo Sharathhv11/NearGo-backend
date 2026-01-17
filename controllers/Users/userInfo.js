fc41a068f8b2fc679d9ff52af270bd67d381c3ba
Sharath <sharathhv88@gmail.com>
2026-01-17 14:34:41 +0530

    changed userinfo controller.

--- controllers/Users/userInfo.js
+++ controllers/Users/userInfo.js
@@ -1,26 +1,38 @@
 import handelAsyncFunction from "../../utils/asyncFunctionHandler.js";
+import CustomError from "./../../utils/customError.js"
+import userModel from "./../../models/userModel.js"
 
 const getUserInfo = handelAsyncFunction(async (req, res, next) => {
   const user = req.user;
+  const {userID} = req.query;
 
+  let target = {}; 
+
+  if( !userID || user._id.toString()  === userID ){
+    target = user;
+  }else{
+    target = await  userModel.findById(userID).lean();
+  }
+ 
   const filteredUser = {
-    id: user._id,
-    email: user.email,
-    name: user.name ?? null,
-    username: user.username ?? null,
-    phone_no: user.phone_no ?? null,
-    interest: user.interest ?? [],
-    role: user.role,
-    verified: user.verified,
-    profilePicture: user.profilePicture,
-    authProvider: user.authProvider,
-    profileCompleted: user.profileCompleted,
+    id:target._id,
+    email:target.email,
+    name:target.name ?? null,
+    username:target.username ?? null,
+    phone_no:target.phone_no ?? null,
+    interest:target.interest ?? [],
+    role:target.role,
+    verified:target.verified,
+    profilePicture:target.profilePicture,
+    authProvider:target.authProvider,
+    profileCompleted:target.profileCompleted,
 
-    createdAt: user.createdAt,
+    createdAt:target.createdAt,
   };
 
   return res.status(200).json({
     status: "success",
+    message:"user information fetched successfully.",
     data: filteredUser,
   });
 });
